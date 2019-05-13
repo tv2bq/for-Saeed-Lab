5a29ddb2ca5030df5262f687ca6f3bc243aa2091
tv2bq <tv2bq@virginia.edu>
2019-05-13 13:39:17 -0400

    Add files via upload

---
+++ programming_practice.h
@@ -0,0 +1,14 @@
+namespace N
+{using namespace std;
+
+	class programming_practice
+	{
+private:
+	std::string sequence;
+	std::string endResult;
+	std::string test;
+public:
+	int main();
+	std::string translate(string);
+};
+}
